00b8912510326db63e4e763865cf651420e8b3b6
lijiliang123 <1651789585@qq.com>
2025-12-24 17:47:43 +0800

    新增文件calcu.py

---
+++ calcu.py
@@ -0,0 +1,43 @@
+'''
+Created by Thomas Lee on 2025/12/14
+这是一个四则运算的计算器小游戏。
+'''
+
+
+tuichu:bool = False
+while tuichu == False:
+    num1 = int(input('请输入第一个数字:  '))
+    num2 = int(input('请输出第二个数字:  '))
+    op = input('请输入操作符:  ')
+    result = 0
+
+
+    def jisuan(num1: object, num2: object, op: object) -> None:
+        if op == '+':
+            result = num1+num2
+            print('您输入的算式是：')
+            print(str(num1) + op + str(num2)+'='+str(result))
+        elif op == '-':
+            result = num1-num2
+            print('您输入的算式是：')
+            print(str(num1) + op + str(num2)+'='+str(result))
+        elif op == '*':
+            result = num1*num2
+            print('您输入的算式是：')
+            print(str(num1) + op + str(num2)+'='+str(result))
+        elif op == '/':
+            result = round(num1/num2,2)  #保留两位小数
+            print('您输入的算式是：')
+            print(str(num1) + op + str(num2)+'='+str(result))
+        else:
+            print('此操作符未定义！请重新输入！')
+
+    jisuan(num1,num2,op)
+
+    someting: str = input('您还要继续计算吗？请输入：Y/N  ')
+    if someting == 'N' or someting == 'n':
+        tuichu = True
+        break
+    else:
+        tuichu = False
+        continue
